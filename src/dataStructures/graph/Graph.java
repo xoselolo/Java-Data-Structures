5c496943937b74b713a8da3136b24abbddcc871e
xoselolo <1a09lopxose@gmail.com>
2019-04-08 01:07:34 +0200

    #1 - Creació de la classe GraphNode

---
+++ src/dataStructures/graph/Graph.java
@@ -0,0 +1,5 @@
+package dataStructures.graph;
+
+public class Graph {
+
+}
---
+++ src/dataStructures/graph/GraphNode.java
@@ -0,0 +1,36 @@
+package dataStructures.graph;
+
+import java.util.ArrayList;
+
+public class GraphNode <T>{
+    // Attributes
+    private T element;
+    private ArrayList<GraphNode<T>> next;
+
+    // Constructors
+    public GraphNode(T element){
+        this.element = element;
+        this.next = new ArrayList<GraphNode<T>>();
+    }
+    public GraphNode(T element, ArrayList<GraphNode<T>> next){
+        this.element = element;
+        this.next = new ArrayList<GraphNode<T>>(next);
+    }
+
+    // Getters & Setters
+    public T getElement() {
+        return element;
+    }
+    public void setElement(T element) {
+        this.element = element;
+    }
+    public ArrayList<GraphNode<T>> getNext() {
+        return next;
+    }
+    public void setNext(ArrayList<GraphNode<T>> next) {
+        this.next = next;
+    }
+
+    // Functions
+
+}
