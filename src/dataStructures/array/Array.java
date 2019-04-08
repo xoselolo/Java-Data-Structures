dbff19bb4a0209156e1c337aa0af641b49872793
sitoftonic <sitoftonic@gmail.com>
2019-04-08 23:30:22 +0200

    feat: Added remove method by index

--- src/dataStructures/array/Array.java
+++ src/dataStructures/array/Array.java
@@ -2,6 +2,7 @@ package dataStructures.array;
 
 import java.util.Arrays;
 
+
 public class Array<T> {
     // Attributes
     private Object[] elements;
@@ -27,6 +28,35 @@ public class Array<T> {
     public void add(T newElement){
         this.elements = Arrays.copyOf(elements, elements.length + 1);
         this.elements[elements.length - 1] = newElement;
+    }
+
+    public void remove(T element) {
+
+    }
+
+    public boolean remove(int index) {
+        int newSize = elements.length - 1;
+        Object[] newArray = new Object[newSize];
+
+        if (index <= newSize && index >= 0) {
+            // If the deleted value is last index
+            if (index == newSize) {
+                newArray = Arrays.copyOf(elements,elements.length - 1);
+            }
+            else {
+                for (int i = 0; i < index; i++) {
+                    newArray[i] = elements[i];
+                }
+                for (int i = index + 1; i < newSize; i++) {
+                    newArray[i] = elements[i];
+                }
+            }
+            elements = newArray;
+            return true;
+        }
+        else {
+            return false;
+        }
     }
     public Object get(int index){
         return elements[index];
