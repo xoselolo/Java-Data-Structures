991743049207acffbfa2a142e6f2a88e83b76cd0
xoselolo <1a09lopxose@gmail.com>
2019-04-15 12:45:20 +0200

    #5 - Reestructuración classe Array
    JavaDoc de funciones

--- src/dataStructures/graph/Graph.java
+++ src/dataStructures/graph/Graph.java
@@ -28,38 +28,4 @@ public class Graph<T> {
         return (GraphNode<T>)getAdjacencyList().get(x);
     }
 
-    public static void main(String[] args) {
-        Graph<User> userGraph = new Graph<User>();
-
-        Array<String> to_follow = new Array<String>();
-        to_follow.add("sitoftonic");
-        to_follow.add("oleksiyPutoAmo");
-
-        User user = new User("xoselolo", 123, to_follow);
-
-        userGraph.add(new GraphNode<User>(user)); // Afegim user "xoselolo"
-
-        Array<GraphNode<User>> array = new Array<GraphNode<User>>();
-        array.add(new GraphNode<User>(user));
-
-        to_follow.add("albertpv");
-
-        user = new User("elTeteFeka", 1314, to_follow);
-
-        GraphNode<User> node = new GraphNode<User>(user, array);
-        userGraph.add(node);
-
-        int connections0 = userGraph.getNodeX(0).getNext().size();
-        System.out.println("Index 0 connects with: " + connections0 + " elements");
-
-        int connections1 = userGraph.getNodeX(1).getNext().size();
-        System.out.println("Index 0 connects with: " + connections1 + " elements");
-
-        Array<GraphNode<User>> connections = userGraph.getNodeX(1).getNext();
-        for (int i = 0; i < connections.size(); i++){
-            String username = ((GraphNode<User>)connections.get(i)).getElement().getUsername();
-            System.out.println("Connected with " + username);
-        }
-    }
-
 }
