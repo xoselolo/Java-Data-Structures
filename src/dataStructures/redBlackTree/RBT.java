c82beca5083c2c8d05cb77e23ff7006d1f68dd9e
xoselolo <1a09lopxose@gmail.com>
2019-04-18 21:25:43 +0200

    #3 - RedBlackTree INSERTION + ROTATION
    TODO removed

--- src/dataStructures/redBlackTree/RBT.java
+++ src/dataStructures/redBlackTree/RBT.java
@@ -121,7 +121,6 @@ public class RBT <T>{
                 }
 
 
-                // TODO: Make Rotations and colorChanging if necessary
                 if (cas1(pointer)){
                     cas1Rotation(pointer);
 
