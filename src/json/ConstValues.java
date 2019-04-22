74b6856267bf066670b3cc76a98c070c3ef53060
xoselolo <1a09lopxose@gmail.com>
2019-04-23 01:07:42 +0200

    #14 - ConstValues for user's jsons

---
+++ src/json/ConstValues.java
@@ -0,0 +1,11 @@
+package json;
+
+public abstract class ConstValues {
+
+    // Users Json File const values
+    public static final String JSON_USERNAME_TAG = "username";
+    public static final String JSON_CREATION_TAG = "creation";
+    public static final String JSON_TO_FOLLOW_TAG = "to_follow";
+
+
+}
