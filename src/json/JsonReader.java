2f3b199752dace64adc0b0a66e45e97ae1ce7960
xoselolo <1a09lopxose@gmail.com>
2019-04-23 01:11:17 +0200

    #12 - JsonReader function to read users from a users .json format file.
    Need to be reviewed and tested.

--- src/json/JsonReader.java
+++ src/json/JsonReader.java
@@ -1,4 +1,41 @@
 package json;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonParser;
+import dataStructures.array.Array;
+import model.Post;
+import model.User;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class JsonReader {
+
+    // FILENAMES
+    public static String USERS_FILENAME = "files/users";
+    public static String POSTS_FILENAME = "files/posts";
+
+    public static Array<User> readUsers() throws FileNotFoundException {
+        Array<User> users = new Array<User>();
+
+        JsonParser parser = new JsonParser();
+        JsonArray usersJsonArray = (JsonArray) parser.parse(new FileReader(USERS_FILENAME));
+
+        int jsonSize = usersJsonArray.size();
+        for (int i = 0; i < jsonSize; i++){
+            users.add(new User(usersJsonArray.get(i).getAsJsonObject()));
+        }
+
+        return users;
+    }
+    public static Array<Post> readPosts() throws FileNotFoundException {
+        Array<Post> posts = new Array<Post>();
+
+        JsonParser parser = new JsonParser();
+        JsonArray postsJsonArray = (JsonArray) parser.parse(new FileReader(POSTS_FILENAME));
+
+        // TODO
+
+        return posts;
+    }
 }
