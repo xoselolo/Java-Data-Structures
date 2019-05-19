0e803036869ba5735a0c54d3daf06d0569c2924a
sitoftonic <sitoftonic@gmail.com>
2019-05-20 00:28:06 +0200

    fix: Solved compilation errors

--- src/dataStructures/rTree/RTree.java
+++ src/dataStructures/rTree/RTree.java
@@ -1,11 +1,5 @@
 package dataStructures.rTree;
 
-import dataStructures.array.Array;
-import json.JsonReader;
-import model.Post;
-
-import java.io.FileNotFoundException;
-
 
 public class RTree {
 
@@ -19,31 +13,4 @@ public class RTree {
         return root;
     }
 
-    public static void main (String[] args){
-
-        JsonReader jr = new JsonReader();
-        RTree rt = new RTree();
-
-        try {
-            Array<Post> posts = jr.readPosts();
-
-            for (int i = 0; i < posts.size(); i++){
-                Post p  = (Post)posts.get(i);
-                rt.root.insertPoint(p, p.getLocation().getLatitude(), p.getLocation().getLongitude());
-            }
-
-        } catch (FileNotFoundException e) {
-            System.out.println("Impossible llegir le fitxer");
-        }
-
-//        rt.root.insertPoint(new Post(0,0), 0, 0);
-//        rt.root.insertPoint(new Post(1,1), 1, 1);
-//        rt.root.insertPoint(new Post(2,2), 2, 2);
-//        rt.root.insertPoint(new Post(3,3), 3, 3);
-//        rt.root.insertPoint(new Post(4,4), 4, 4);
-//        rt.root.insertPoint(new Post(5,5), 5, 5);
-//        rt.root.insertPoint(new Post(6,6), 6, 6);
-
-        System.out.println("funciona?");
-    }
 }
